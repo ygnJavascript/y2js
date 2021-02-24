fef03bb07776e0154fab3001f4b5dbdd89a476fe
ygnJavascript <78275383+ygnJavascript@users.noreply.github.com>
2021-02-24 16:55:42 +0300

    Add files via upload

---
+++ y2js.js
@@ -0,0 +1,15 @@
+function y(x,y=""){
+	if(x.indexOf(" ")==-1){
+    	if(y!==""){
+        	return document.querySelectorAll(x)[y];
+        }else{
+    		return document.querySelector(x);
+        }
+    }else{
+    	if(y!==""){
+    		return document.getElementsByClassName(x)[y];
+        }else{
+        	return document.getElementsByClassName(x)[0];
+        }
+    }
+}
