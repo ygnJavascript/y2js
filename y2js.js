069add0f7f4d74f77557ca5df12121d9250b9ade
ygnJavascript <78275383+ygnJavascript@users.noreply.github.com>
2021-02-24 19:03:37 +0300

    Update y2js.js

--- y2js.js
+++ y2js.js
@@ -1,19 +1,19 @@
 function y(x,y=null){
-	if(y=="k"){
-		return document.getElementsByClassName(x);
-	}else{
-		if(x.indexOf(" ")==-1){
-			if(y!==null){
-				return document.querySelectorAll(x)[y];
-			}else{
-				return document.querySelector(x);
-			}
+	if(x.indexOf(" ")==-1){
+		if(y!==null){
+			return document.querySelectorAll(x)[y];
 		}else{
-			if(y!==null){
-				return document.getElementsByClassName(x)[y];
-			}else{
-				return document.getElementsByClassName(x);
-			}
+			return document.querySelector(x)[0];
+		}
+	}else{
+		if(y!==null){
+			return document.getElementsByClassName(x)[y];
+		}else{
+			return document.getElementsByClassName(x)[0];
 		}
 	}
+}
+
+function k(x){
+	return document.getElementsByClassName(x);
 }
